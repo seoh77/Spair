f309a91874c3bdfb1d16c57d46eb8197e06c2202
ssafy11thseoul <ssaf010203@gmail.com>
2024-05-12 02:18:37 +0900

    :sparkles: Feat: Footer 생성 및 구헌

--- front/spair-project/src/App.vue
+++ front/spair-project/src/App.vue
@@ -3,12 +3,14 @@
   <div id="container">
     <TheHeaderNav/>
     <RouterView />
+    <TheFooter/>
   </div>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import TheHeaderNav from '@/components/common/TheHeaderNav.vue'
+import TheFooter from '@/components/common/TheFooter.vue';
 </script>
 
 <style scoped>
---
+++ front/spair-project/src/components/common/TheFooter.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="footer-container">
+        <footer>
+            <a href="/">개인정보처리방침</a>
+            <a href="/">회사소개</a>
+            <a href="/">고객센터</a>
+            <a href="/">&copySPAIR</a>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    #footer-container {
+        width: 100%;
+        height: 200px;
+        background-color: var(--secondary-color);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: 'NanumSquareRound';
+    }
+    footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    a {
+        font-size: 1.2rem;
+        font-weight: bold;
+        text-decoration: none;
+        color: black;
+        margin: 10px;
+    }
+</style>
