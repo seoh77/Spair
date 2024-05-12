38c4b874ad8f2795159751cb54750aae4d25e33b
ssafy11thseoul <ssaf010203@gmail.com>
2024-05-13 00:06:12 +0900

    :sparkles: Feat: HomeView의 게시판란 임시 구현(BoardView)

--- front/spair-project/src/views/BoardView.vue
+++ front/spair-project/src/views/BoardView.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
+    <div id="boardview-container">
+        <!-- 추후 BoardView의 자식 컴포넌트(BoardList 등)를 구현해서 라우터로 연결할 예정 
+        현 단계에서는 편의를 위해 BoardView에 임시로 페이지 구현 -->
         <h2>BoardView입니다.</h2>
     </div>
 </template>
@@ -7,5 +9,15 @@
 <script setup>
 </script>
 
-<script setup>
-</script>
+<style scoped>
+    #boardview-container {
+        width: 75%;
+        max-width: 1200px;
+        min-width: 1000px;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        font-family: 'Tenada';
+        margin: 20px;
+    }
+</style>
--- front/spair-project/src/views/HomeView.vue
+++ front/spair-project/src/views/HomeView.vue
@@ -3,12 +3,19 @@
   <main>
       <Intro/>
       <Advertisement/>
+      <div id="title">
+        최신 PAIR 찾기글
+      </div>
+       <!-- 추후 BoardView의 자식 컴포넌트(BoardList 등)를 구현해서 라우터로 연결할 예정 
+        현 단계에서는 편의를 위해 BoardView에 임시로 페이지 구현 -->
+      <BoardView/>
   </main>
 </template>
 
 <script setup>
 import Intro from '@/components/home/Intro.vue'  
 import Advertisement from '@/components/home/Advertisement.vue';
+import BoardView from '@/views/BoardView.vue';
 </script>
 
 <style scoped>
@@ -16,7 +23,28 @@ import Advertisement from '@/components/home/Advertisement.vue';
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 100%;
-    
+    width: 100%;  
+  }
+  #title {
+    width: 75%;
+    max-width: 1200px;
+    min-width: 1000px;
+    display:flex;
+    justify-content: start;
+    align-items: center;
+    font-family: 'Tenada';
+    font-size: 2rem;
+    margin: 40px;
+  }
+  main:nth-child(4) {
+    width: 75%;
+    max-width: 1200px;
+    min-width: 1000px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Tenada';
+    font-size: 2rem;
+    margin: 40px;
   }
 </style>
