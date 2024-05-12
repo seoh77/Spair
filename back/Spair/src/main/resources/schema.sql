42de3d957b9fa5fee518948ebf5d5217b5e23155
seoh77 <seoh7dev@gmail.com>
2024-05-12 22:00:24 +0900

    🚚 Rename: DB 명명규칙에 따라 테이블명 수정

--- back/Spair/src/main/resources/schema.sql
+++ back/Spair/src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE spair ;
 
 USE spair ;
 
-CREATE TABLE `Users` (
+CREATE TABLE `user` (
 	`user_id`	BIGINT	NOT NULL  AUTO_INCREMENT,
 	`login_id`	VARCHAR(20)	NOT NULL  UNIQUE,
 	`password`	VARCHAR(20)	NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE `Users` (
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE `SportsCenter` (
+CREATE TABLE `sports_center` (
 	`center_id`	BIGINT	NOT NULL  AUTO_INCREMENT,
 	`road_address`	VARCHAR(100)	NOT NULL,		-- 도로명 주소
 	`local_address`	VARCHAR(100)	NOT NULL,		-- 지번 주소
@@ -26,7 +26,7 @@ CREATE TABLE `SportsCenter` (
     PRIMARY KEY (center_id)
 );
 
-CREATE TABLE `Post` (
+CREATE TABLE `post` (
 	`post_id`	BIGINT	NOT NULL  AUTO_INCREMENT,
 	`user_id`	BIGINT	NOT NULL,
 	`title`	VARCHAR(100)	NOT NULL,
@@ -40,11 +40,11 @@ CREATE TABLE `Post` (
 	`gender`	TINYINT(1)	NOT NULL,					-- 모집성별 : 남자(1), 여자(2), 상관없음(3)
 	`recruitment_num`	VARCHAR(10)	NOT NULL,			-- 모집인원 : 1명, 2명, 3명 이상, 상관없음
     PRIMARY KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (center_id) REFERENCES SportsCenter(center_id)
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (center_id) REFERENCES sports_center(center_id)
 );
 
-CREATE TABLE `Comments` (
+CREATE TABLE `comment` (
 	`comment_id`	BIGINT	NOT NULL  AUTO_INCREMENT,
 	`post_id`	BIGINT	NOT NULL,
 	`user_id`	BIGINT	NOT NULL,
@@ -54,6 +54,6 @@ CREATE TABLE `Comments` (
 	`modified_date`	TIMESTAMP	NULL,
 	`parent_id`	BIGINT	NULL,					-- 부모 댓글 ID
     PRIMARY KEY (comment_id),
-    FOREIGN KEY (post_id) REFERENCES Post(post_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (post_id) REFERENCES post(post_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
