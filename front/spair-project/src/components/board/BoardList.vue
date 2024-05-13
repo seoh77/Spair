19d3e2345b97490311a09db1e6b332d0ef7c365e
ssafy11thseoul <ssaf010203@gmail.com>
2024-05-13 18:05:09 +0900

    :sparkles: Feat: BoardList 구현

--- front/spair-project/src/components/board/BoardList.vue
+++ front/spair-project/src/components/board/BoardList.vue
@@ -1,9 +1,68 @@
 <template>
-    <div>BoardList입니다.</div>
+    <div id="list-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>번호</th>
+                    <th>제목</th>
+                    <th>작성자</th>
+                    <th>작성시각</th>
+                    <th>모집상태</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="board in store.boardList" :key="board.id">
+                    <!-- 여기에 camelcase로 쓴 이유? 자바에서 데이터 넘겨줄 때 카멜케이스로 줬기 때문-->
+                    <td>{{ board.id }}</td>
+                    <!-- <td>
+                        <RouterLink :to="`/board/${board.id}`">{{ board.title }}</RouterLink>
+                    </td> -->
+                    <td>{{  board.title }}</td>
+                    <td>{{  board.writer }}</td>
+                    <td>{{  board.regDate }}</td>
+                    <td>{{  board.status }}</td>
+                </tr>
+            </tbody>
+        </table>
+        
+    </div>
 </template>
 
 <script setup>
+import { useBoardStore } from '@/stores/board';
+const store = useBoardStore();
+
 </script>
 
 <style scoped>
+#list-container {
+    width: 75%;
+    max-width: 1200px;
+    min-width: 1000px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Tenada';
+    margin: 2rem;
+    border: 0.1rem solid var(--gray-color);
+    box-shadow: 0.1rem 0.5rem 0.5rem var(--gray-color);
+}
+table {
+    width: 90%;
+    height: 200px;
+    margin: 3rem 0;
+    table-layout: fixed;
+}
+thead, tbody {
+    text-align: center;
+    font-size: 1.5rem;
+}
+td {
+    font-size: 1.3rem;
+    font-family: 'NanumSquareRound';
+    border-bottom: 0.1rem dotted var(--primary-color);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
 </style>
--- front/spair-project/src/stores/board.js
+++ front/spair-project/src/stores/board.js
@@ -2,11 +2,30 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useBoardStore = defineStore('board', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+   // API 연결 전 임시 데이터 
+   const boardList = ref([
+    {
+      id: 1,
+      title: "test1111111111111111111111111111111111111111111111111",
+      writer: "tester1",
+      regDate: "2024.05.11",
+      status: false,
+    },
+    {
+      id: 2,
+      title: "제목이 길 때 ... 으로 표시될 고양",
+      writer: "nyang",
+      regDate: "2024.05.12",
+      status: true,
+    },
+    {
+      id: 3,
+      title: "test3343333333333333333333",
+      writer: "작성자도 길면 어떻게 되는거지",
+      regDate: "2024.05.13",
+      status: true,
+    }
+  ])
 
-  return { count, doubleCount, increment }
+  return { boardList }
 })
