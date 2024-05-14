0da7cd31d6a304812ab1b2c454c04c899de5fa5d
ssafy11thseoul <ssaf010203@gmail.com>
2024-05-14 11:51:14 +0900

    :lipstick: Design: BoardView CSS및 사용자 UI 변경

--- front/spair-project/src/views/BoardView.vue
+++ front/spair-project/src/views/BoardView.vue
@@ -3,12 +3,15 @@
         <div id="title">우리동네 PAIR 찾기 게시판</div>
         
         <RouterView/>
-        <RouterLink :to="{ name: 'boardCreate' }">게시글 등록</RouterLink>
+        <div v-if="$route.path !== '/board/create'" id="link">
+            <RouterLink :to="{ name: 'boardCreate' }">게시글 등록</RouterLink>
+        </div>
     </div>
 </template>
 
 <script setup>
 import { RouterView, RouterLink } from 'vue-router';
+
 </script>
 
 <style scoped>
@@ -29,5 +32,25 @@ import { RouterView, RouterLink } from 'vue-router';
         align-items: center;
         font-family: 'Tenada';
         margin: 2rem;
+    }
+    #link {
+        width: 75%;
+        max-width: 1200px;
+        min-width: 1000px;
+        display: flex;
+        flex-direction: row-reverse;
+    }
+    #link a {
+        width: 15%;
+        height: 2.4rem;
+        font-family: 'NanumSquareRound';
+        font-weight: bold;
+        font-size: 1.4rem;
+        text-align: center;
+        text-decoration: none;
+        background-color: var(--primary-color);
+        color: #FFFFFF;
+        border-radius: 0.5rem;
+        margin: 2rem 0;
     }
 </style>
